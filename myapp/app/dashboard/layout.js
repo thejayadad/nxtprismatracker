76cfe6aa2452fd2942cfb4aa85348125a1166248
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-29 17:08:40 -0500

    schemas updated

--- myapp/app/dashboard/layout.js
+++ myapp/app/dashboard/layout.js
@@ -3,9 +3,19 @@ import React from 'react'
 
 const layout = ({children}) => {
   return (
-    <div>
+    <div
+    className="
+             flex
+             min-h-screen
+             w-full
+             flex-col
+             items-center
+   "
+    >
         <Navbar />
-        {children}
+        <main className="flex flex-grow w-full justify-center items-center">
+            {children}
+        </main>
     </div>
   )
 }
--- myapp/app/dashboard/page.js
+++ myapp/app/dashboard/page.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import getServerUser from '@/lib/getServerUser'
+import React, { Suspense } from 'react'
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
   return (
-    <div>DashboardPage</div>
+    <>
+    <Suspense fallback={<WelcomeFallback />}>
+        <WelcomeMessage />
+    </Suspense>
+    </>
   )
+}
+
+async function WelcomeMessage(){
+    const user = await getServerUser()
+    const email = user.email
+    if(!user){
+        return <h2 className='text-center'>Error Accessing</h2>
+    }
+    return (
+        <div className="flex w-full mb-12">
+            <h1 className='text-4xl font-bold'>
+                Welcome Back, <br /> {email}
+            </h1>
+        </div>
+    )
+}
+function WelcomeFallback(){
+    return (
+        <div className="flex w-full mb-12">
+              <h1 className="text-4xl font-bold">
+                Loading....
+              </h1>            
+        </div>
+    )
 }
 
 export default DashboardPage
